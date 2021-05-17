861d1ebde5f2b184cca7ef0fabd256989855580e
guirodg <guizinhord@hotmail.com>
2021-05-17 16:55:50 -0300

    feat: criação dos modelos que vao representar nossos eventos

---
+++ src/main/java/com/app/aws_project01/model/Envelope.java
@@ -0,0 +1,22 @@
+package com.app.aws_project01.model;
+
+public class Envelope {
+    private String eventType;
+    private String data;
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
